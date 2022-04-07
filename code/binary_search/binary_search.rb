1470be03e50e3ec6619235c39eefd024fe08c931
Chris Clifton <chris.clifton@rtslabs.com>
2022-04-07 12:19:25 -0400

    update comments on binary_search.rb

--- code/binary_search/binary_search.rb
+++ code/binary_search/binary_search.rb
@@ -1,19 +1,19 @@
 # Notes:
-# First iteration. This works, but now that its done, its pretty apparent that
-# it has room for improvement. This should be refactoredto use index pointers
+# First iteration. This works*, but now that its done, its pretty apparent that
+# it has room for improvement. This should be refactored to use index pointers
 # instead of slicing and modifying the array passed in.
 # This should also be refactored to return either the search_target (if we find it)
 # or nil and then get rid of @found and @result.
 
-# TODO: Is having @steps += 1 at the bottom correct? Wouldn't that mean that if we
-#       land on the @search_target as the first current_index that we'd have 0 steps?
-#       Need to figure that out before I say this "works".
+# TODO: Having @steps += 1 at the end of the loop cant be right. That would mean
+#       that if we land on the @search_target as the first current_index that
+#       we'd have 0 steps? Need to fix that out before I say this "works".
 
 # Given an array of numbers, perform a binary search and return the number of
 # steps it takes to complete.
 #
-# ex: binary_search = BinarySearch.new(search_target: 4, ordered_array: [1, 2, 3, 4, 5, 6]).perform
-#     binary_search => 4
+# ex: binary_search = BinarySearch.new(search_target: 4, ordered_array: [1, 2, 3, 4, 5, 6])
+#     binary_search.perform => 4
 #     binary_search.steps => 1
 class BinarySearch
   attr_accessor :steps, :found, :result
